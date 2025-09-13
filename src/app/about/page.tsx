329f40d6adac4155e852f701f6cab562a1d0998c
gojoego <josephjgallego@gmail.com>
2025-09-13 12:27:17 -0500

    about page v1

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function AboutMe() {
+    return (
+        <div className="container mx-auto px-4 py-16 max-w-3xl">
+            <h1 className="text-4xl font-bold tracking-tight mb-6">
+                ABOUT ME 
+            </h1>
+            <p className="text-lg leading-relaxed text-neutral-700 mb-6">
+               Hi! My name is Diana and I specialize in landscape oil paintings. 
+            </p>
+            <p className="text-lg leading-relaxed text-neutral-700 mb-6">
+                I am based in Texas. 
+            </p>
+        </div>
+    )
+}
